887dbd409e148db0c79d38caa42875aea58814d6
biswa-ganguly <biswaganguly16@gmail.com>
2025-05-18 10:19:26 +0530

    backend setup

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -1,9 +1,12 @@
+"use client"
 import React from 'react'
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 function Provider({children}) {
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
   return (
     <div>
-        {children}
+        <ConvexProvider client={convex}>{children}</ConvexProvider>
     </div>
   )
 }
---
+++ convex/schema.js
@@ -0,0 +1,10 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+        userName: v.string(),
+        userEmail: v.string(),
+        imageUrl: v.string()
+    })
+})
